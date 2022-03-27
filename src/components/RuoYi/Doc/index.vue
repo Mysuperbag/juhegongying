f5547749f33e53b3d60f29cdc8703399c94a3419
朱海荣 <1747938352@qq.com>
2022-03-27 16:29:17 +0800

    聚合供应

---
+++ src/components/RuoYi/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cwDoc',
+  data() {
+    return {
+      url: 'http://doc.cw.vip/cw-vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ src/components/RuoYi/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cwGit',
+  data() {
+    return {
+      url: 'https://gitee.com/y_project/cw-Vue'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
